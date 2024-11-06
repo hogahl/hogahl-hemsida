2233189e836a2ff40ca239904bd464c6522ee3d2
Hugo Ahlberg <hugahl02@elevad.umea.se>
2024-11-06 13:50:23 +0100

    photoapp klar

---
+++ docs/_app/immutable/entry/start.Drokme3V.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DA9rYxEl.js";export{t as start};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,11 +20,12 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { base } from '$app/paths';
 </script>
 
 <nav>
-	<a href="/photoapp">Photo App</a>
-	<a href="/">Hem</a>
+	<a href="{base}/photoapp">Photo App</a>
+	<a href="{base}/">Hem</a>
 </nav>
 
 <slot />
