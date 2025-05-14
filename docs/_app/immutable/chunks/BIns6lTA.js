c6891e4638be1fbfcbf09120dd7b180e10fd3bc6
Hugo Ahlberg <hugahl02@elevad.umea.se>
2025-05-14 14:26:42 +0200

    message

---
+++ docs/_app/immutable/chunks/BIns6lTA.js
@@ -0,0 +1,9 @@
+import{s,n as e}from"./DykIVeQB.js";import{S as o,i,d as m,a as d,c as g,l as k,h as v}from"./CfWegfSe.js";function p(r){let a,n=`Nu har jag gjort klart även det andra spelet inom bilar kategorin.
+Det är ett spel där man väljer mellan två bilar vilken av de som är dyrast.
+Får man rätt så får man en poäng medans ifall det är fel så börjar man från början.
+Man kan även skippa max tre gånger ifall man känner att man inte vågar gissa.
+Efter det så har jag börjat på kategorin mat som då är väldigt lik det andra spelet om bilar.
+Man ska välja vilken produkt som flest var ute efter år 2024.
+Man kan även välja i vilket land mellan Sverige, Danmark och Norge.
+Sverige och Danmark är helt klara men Norge är nästan klar.
+Nästa lektion ska jag alltså göra klart allt inom mat och sedan börja på nästa kategori.`;return{c(){a=v("p"),a.textContent=n},l(t){a=g(t,"P",{"data-svelte-h":!0}),k(a)!=="svelte-1hduyw3"&&(a.textContent=n)},m(t,l){d(t,a,l)},p:e,i:e,o:e,d(t){t&&m(a)}}}const f={title:"Tredje bloggen",date:"2025-05-07"},{title:h,date:j}=f;class b extends o{constructor(a){super(),i(this,a,null,p,s,{})}}export{b as default,f as metadata};
---
+++ docs/_app/immutable/entry/start.CQj_NdLi.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/C_rGnZyR.js";export{a as start};
