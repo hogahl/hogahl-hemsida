59992d9ad055b548a9717a7788dc7d5a27702c9d
Hugo Ahlberg <hugahl02@elevad.umea.se>
2025-05-26 15:29:19 +0200

    message

---
+++ docs/_app/immutable/chunks/pUY37SKj.js
@@ -0,0 +1,12 @@
+import{s as l,n as a}from"./DykIVeQB.js";import{S as o,i as d,d as i,a as m,c as g,l as v,h as k}from"./CfWegfSe.js";function p(r){let e,n=`Jag har slutlitgen gjort klart hela kategorin mat genom att kopiera in samma kod från andra kategorier men bytt namn på variabler och funktioner.
+Efter det började jag med kategorin musik som även den är klar nu.
+Den gjorde jag genom att skriva om bilar koden.
+Jag behövde hitta bilder på internet som jag kunde använda men annars var det inte så mycket programmerande.
+Ena spelet på musik är alltså ett spel där man väljer vilken av två artister som har flest månadslyssnare.
+Jag har lyckats göra det genom att använda funktioner som kollar vilken av artisterna som väljs.
+Sedan kollar funktionerna ifall den man valde har ett mindre värde än den andra artisten.
+Ifall den har det så betyder det att man valt rätt svar och därmed kommer två nya artister upp.
+Väljer den artist som har ett högre värde så är det fel och då börjar spelet om.
+Värdet är på vilken plats artisten har i listan.
+Ju lägre värde desto fler månadslyssnare.
+Nu återstår endast att göra sista kategorin, sociala medier.`;return{c(){e=k("p"),e.textContent=n},l(t){e=g(t,"P",{"data-svelte-h":!0}),v(e)!=="svelte-17n2t9n"&&(e.textContent=n)},m(t,s){m(t,e,s)},p:a,i:a,o:a,d(t){t&&i(e)}}}const u={title:"Fjärde bloggen",date:"2025-05-26"},{title:f,date:j}=u;class b extends o{constructor(e){super(),d(this,e,null,p,l,{})}}export{b as default,u as metadata};
---
+++ docs/_app/immutable/entry/start.Co-RGcbl.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/CEXv67IO.js";export{a as start};
